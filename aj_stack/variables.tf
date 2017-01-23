72d38a7dadc729f975141f7e5361c0e0380cdbb8
arunsanna <arun.sanna@reancloud.com>
2017-01-23 00:59:10 -0500

    updated the teraform code to deploy AJ development Instance

---
+++ aj_stack/variables.tf
@@ -0,0 +1,47 @@
+variable ami_id {
+  default = "ami-e13739f6"
+}
+
+variable key_name {
+  default = "arun_rean_trainee"
+}
+
+variable security_group {
+  default = "sg-806bcbfc"
+}
+
+variable instance_type {
+  default = "t2.medium"
+}
+
+variable az {
+  default = "us-east-1a"
+}
+
+variable owner {
+  default = "arun.sanna"
+}
+
+variable name {
+  default = "AJ-Development"
+}
+
+variable remote_state_bucket {
+  default = "arun-cloudfront-logs"
+}
+
+variable remote_state_key {
+  default = "tfstate/aj-infra"
+}
+
+variable remote_buc_reg {
+  default = "us-east-1"
+}
+
+variable expiration_date {
+  default = "2017-01-30"
+}
+
+variable "iamprofile" {
+  default = "s3accesstoec2"
+}
---
+++ jenkins/main.tf
@@ -0,0 +1,8 @@
+#create two security Groups
+#Shell with all the scripts to install packages
+#create an instance and Use Security Group of the instance
+#create ELB
+#add ELB Security Group to ELB
+#Add Instance to ELB
+#add Shell to Launch Configuration
+#Add ASG and Attach to LC and Attach to ELB
